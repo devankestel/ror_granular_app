094fda154d09e0d2d871cebe0ba7167933647832
Devan Kestel <devan.kestel@gmail.com>
2015-06-25 00:31:46 -0400

    Added user_login_or_logout to application helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_login_or_logout
+    if session[:user]
+      content_tag(:p, "Currently logged in as 
+                       #{session[:user]}") +
+      form_tag('/log_out', method: :delete) do 
+        submit_tag 'log out' 
+      end
+    else 
+      link_to 'log in', '/login' 
+    end
+  end
 end
