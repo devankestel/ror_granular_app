11772165eb59c5a5469c7a2aa418697655cd768f
Devan Kestel <devan.kestel@gmail.com>
2015-06-25 00:35:38 -0400

    Added new view to Sessions and routes.
    Added new view with login form. Added new, create, and destroy routes
    for sessions to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get '/login' => 'sessions#new'
+
+  post '/login' => 'sessions#create'
+  delete '/log_out' => 'sessions#destroy'
+
   root 'restaurants#index'
 
   resources :restaurants
