4467660ce3b12864696aa731f3531898d4ebb725
Devan Kestel <devan.kestel@gmail.com>
2015-06-25 00:28:13 -0400

    Added sessions controller actions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user])
+    if user && user.password == params[:password]
+      session[:user] = params[:user]
+      redirect_to '/index', notice: '#{user} has successfully logged in.'
+    else
+      flash.now[:error] = "Incorrect user name or password. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user] = nil
+    redirect_to '/index', notice: 'You have successfully logged out.'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user])
+    if user && user.password == params[:password]
+      session[:user] = params[:user]
+      redirect_to '/page_1', notice: '#{user} has successfully logged in.'
+    else
+      flash.now[:error] = "Incorrect user name or password. Please try again."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user] = nil
+    redirect_to '/page_1', notice: 'You have successfully logged out.'
+  end
 end
