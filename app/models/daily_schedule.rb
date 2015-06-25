f8374c4f1fa3dd10a44d8ada29bff238cd2aa92d
Devan Kestel <devan.kestel@gmail.com>
2015-06-25 01:32:15 -0400

    Added association between daily_schedule and restaurant models
    Restaurant has_many DailySchedules
    
    DailySchedule belongs_to Restaurant.
    
    Moved serialization of hours to DailySchedule table.

--- app/models/daily_schedule.rb
+++ app/models/daily_schedule.rb
@@ -1,2 +1,13 @@
 class DailySchedule < ActiveRecord::Base
+  belongs_to :restaurant
+
+  serialize :hours   ### gets a more complicated data structure to be able to be inputed into the database
+  before_create :set_hours
+
+
+  protected
+
+    def set_hours
+      self.hours ||= []
+    end
 end
